611a0a176b0fdb69f5b244858babfd9a339167dd
eneszzeybek <eneszzeybek@gmail.com>
2021-02-23 21:26:09 +0300

    Commit

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            CarManager carManager = new CarManager(new InMemoryCarDal()); // Add Project Reference to -> Data Access, Entities and Business
+            CarManager carManager = new CarManager(new InMemoryCarDal()); // Add Project Reference to Data Access, Entities and Business
             foreach (var car in carManager.GetAll())
             {
                 Console.WriteLine(car.Description);
